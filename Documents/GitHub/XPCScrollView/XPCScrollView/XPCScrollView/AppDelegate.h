8574747d1d6366ee8a56fe46b8c3969949f93c56
15317937361 <1092417246@qq.com>
2018-05-18 17:40:55 +0800

    XPCScrollView

---
+++ Documents/GitHub/XPCScrollView/XPCScrollView/XPCScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XPCScrollView
+//
+//  Created by xpc on 2018/5/17.
+//  Copyright © 2018年 xpc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
